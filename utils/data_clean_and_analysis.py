2921b8031b2704ebe112a24a41995f71d5201da5
Huihan Yang <95757902+foxintohumanbeing@users.noreply.github.com>
2023-02-24 12:43:20 +0800

    modify the part of gmm and data_clean_and_analysis

--- utils/data_clean_and_analysis.py
+++ utils/data_clean_and_analysis.py
@@ -1,1 +1,63 @@
 import numpy as np
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+
+def remove_id():
+    """do not use this function """
+    train = pd.read_csv('data/train.csv')
+    test = pd.read_csv('data/test.csv')
+    train.to_csv('data/train_no_id.csv')
+    test.to_csv('data/test_no_id.csv')
+
+def get_data():
+     train = pd.read_csv('data/train_no_id.csv').iloc[:,1:]
+     test = pd.read_csv('data/test_no_id.csv').iloc[:,1:]
+     return train,test
+
+def histogram(data:pd.DataFrame,save = False):
+    """draw the histogram of input data
+        histogram is drawn based on columns 
+    """
+
+    for i in range(len(data.columns)):
+        plt.hist(data.iloc[:,i],bins = 100)
+        plt.title("histogram of "+data.columns[i])
+        if save == True:
+            plt.savefig('figure/histogram_'+str(data.columns[i]))
+        plt.show()
+
+def heatmap(data:pd.DataFrame,save = False):
+    """ draw the heatmap of given df"""
+    sns.heatmap(data.corr())
+    if save == True:
+            plt.savefig('figure/heatmap')
+    plt.show()
+
+def minmax(data:pd.DataFrame,label = False):
+    """ do the minmax normalization based on whether data has y
+        by default we assume that the label is in the last line
+    """
+    if label == True:
+          y = data.iloc[:,-1]
+          data = data.iloc[:,:-1]
+    scaler = MinMaxScaler()
+    data_minmax = pd.DataFrame(columns=data.columns)
+    for i in range(len(data.columns)):
+        scaler.fit(data.iloc[:,i].values.reshape(-1,1))
+        data_minmax.iloc[:,i] = scaler.transform(data.iloc[:,i].values.reshape(-1,1)).reshape(1,-1)[0]
+    if label == True:
+         data_minmax['labl'] = y.values
+    return data_minmax
+
+def result_comparition(data:np.ndarray):
+    """
+    here we input the predicted results and compard with the file submission.csv
+    score is measured by the number of different predictions
+    """
+    a = pd.read_csv("output_record/submission.csv")['prediction'].values
+    score = np.sum(np.abs(a-data))
+    score_rate = score/len(data)
+    return score, score_rate
---
+++ utils/gmm.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.mixture import GaussianMixture
+from utils import data_clean_and_analysis
+
+def i_gmm(train_data, test_data, normal = True, save = "tmp.csv"):
+    """ 
+    train data should not contain columns of lable 
+    both of the input data should not contain columns of id
+    """
+    if normal == True:
+        train_data = data_clean_and_analysis.minmax(train_data)
+        test_data = data_clean_and_analysis.minmax(test_data)
+    
+    gmm = GaussianMixture(n_components=2)
+    gmm.fit(train_data)
+    predicted = gmm.predict(test_data)
+    score, score_rate = data_clean_and_analysis.result_comparition(predicted)
+    if score_rate < 0.1:
+        predicted_df = pd.DataFrame(pd.read_csv('data/test.csv').iloc[:,-1])
+        predicted_df['prediction'] = predicted
+        predicted_df.to_csv(save, index=False)
+    return score,score_rate
+
+def gmm_plot(predicted_data:pd.DataFrame,test_data:pd.DataFrame):
+    colors = ["navy", "turquoise"]
+    tmp = predicted_data.iloc[:,1]
+    test_data['useless_feature'] = np.zeros(len(tmp))
+    test_data['predicted'] = tmp
+    test_matrix = test_data.values
+    for i, color in enumerate(colors):
+        data = test_matrix[test_matrix[:,2] == i][:,0:2]
+        plt.scatter(data[:, 0], data[:, 1], color=color)
+        plt.show()
+
+
